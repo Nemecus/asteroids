560f50dd3fd7bbd1c20cab18a4b40a411e5c168c
Nemecus <7306223+Nemecus@users.noreply.github.com>
2024-08-22 10:27:51 -0500

    added game clock

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+    
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +18,7 @@ def main():
         
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
