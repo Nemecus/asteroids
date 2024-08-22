f72bbdd6799337f53a2371b9ce7fafe159a89860
Nemecus <7306223+Nemecus@users.noreply.github.com>
2024-08-22 11:46:15 -0500

    added player asteroids and bullets

--- main.py
+++ main.py
@@ -1,22 +1,52 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        for obj in updatable:
+            obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.check_collision(player):
+                print("Game over!")
+                sys.exit()
+        
         screen.fill((0, 0, 0))
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
 
